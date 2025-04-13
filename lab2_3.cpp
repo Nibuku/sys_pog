138174d9e2a94a6c3352d2bca0b6cb9df3f6b792
Вероника Будюк <veronika.budyuk2004@gmail.com>
2025-04-13 14:26:15 +0400

    Обновила 3 часть. для корректной обработки ошибок см пред версию

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,4 +1,4 @@
-#include "../sys_pog/check.hpp"
+#include "check.hpp"
 #include <unistd.h>
 #include <wait.h>
 #include <fcntl.h>
@@ -20,53 +20,33 @@ timespec next_timeout(timespec wait_time) {
     return result;
 }
 
-bool process_exists(pid_t p) {
-    if (kill(p, 0) == -1 && errno == ESRCH)
-        return false;
-    return true;
-}
-
-void no_zombie() {
-    struct sigaction s {};
-    s.sa_handler = SIG_IGN;
-    check(sigaction(SIGCHLD, &s, NULL));
-}
-
-void wait_m( mqd_t queue, int attempt_buffer, pid_t opponent, bool direction) {
-    timespec timeout = next_timeout({1, 0});
-    ssize_t r;
-    while (true) {
-        if (direction==0)
-            r = check_except(mq_timedreceive(queue, (char*)&attempt_buffer, sizeof(int), nullptr, &timeout), ETIMEDOUT);
-        else {
-            r = check_except(mq_timedsend(queue, (char*)&attempt_buffer, sizeof(int), 0, &timeout), ETIMEDOUT);}
-
-        if (r >= 0)
-            break;
-
-        if (!process_exists(opponent))
-            exit(EXIT_FAILURE);
-    }
-        timeout = next_timeout({1, 0});
-    }
-
-void left_player(mqd_t queue_out, mqd_t queue_in, int max_range, pid_t opponent) {
-
+void left_player(mqd_t queue_out, mqd_t queue_in, int max_range) {
+    srand(time(0) ^ getpid());
     int number = rand() % max_range + 1;
     printf("Process %d hidden the number: %d\n", getpid(), number);
-    wait_m( queue_out, max_range, opponent, 1);
+    timespec timeout = next_timeout({1, 0});
 
-    int guessed = 0;
-    int attempt_buffer=0;
+    check(mq_timedsend(queue_out, (char*)&max_range, sizeof(int), 0, &timeout));
+
+    bool guessed = false;
+    int attempt_buffer;
     int flag;
 
     while (!guessed) {
+        timespec timeout = next_timeout({1, 0});
+        ssize_t r;
 
-        wait_m(queue_in, attempt_buffer, opponent, 0);
+        while ((r = mq_timedreceive(queue_in, (char*)&attempt_buffer, sizeof(int), nullptr, &timeout)) < 0) {
+            check_except(r, ETIMEDOUT);
+            if (waitpid(0, nullptr, WNOHANG) != 0)
+                exit(0);
+            timeout = next_timeout({1, 0});
+        }
+
         if (attempt_buffer == number) {
             flag = 1;
             check(mq_send(queue_out, (char*)&flag, sizeof(int), 0));
-            guessed = 1;
+            guessed = true;
         } else {
             flag = 2;
             check(mq_send(queue_out, (char*)&flag, sizeof(int), 0));
@@ -75,12 +55,13 @@ void left_player(mqd_t queue_out, mqd_t queue_in, int max_range, pid_t opponent)
 }
 
 
-void right_player(mqd_t queue_out, mqd_t queue_in, pid_t opponent) {
+void right_player(mqd_t queue_out, mqd_t queue_in) {
     int max_range;
-    wait_m( queue_in, max_range, opponent, 0);
+    timespec timeout = next_timeout({1, 0});
+    check(mq_timedreceive(queue_in, (char*)&max_range, sizeof(max_range), nullptr, &timeout));
 
     int flag;
-    int guessed = 0;
+    bool guessed = false;
     int value;
     int count = 0;
     std::stack<int> numbers;
@@ -88,15 +69,15 @@ void right_player(mqd_t queue_out, mqd_t queue_in, pid_t opponent) {
     for (int i = 1; i <= max_range; ++i)
         numbers.push(i);
 
-    while (!guessed && !numbers.empty()) {
+    while (!guessed && !numbers.empty()) {  // Добавлена проверка
         value = numbers.top();
         numbers.pop();
 
-        wait_m( queue_out, value, opponent, 1);
-        wait_m( queue_in, flag, opponent, 0);
+        check(mq_timedsend(queue_out, (char*)&value, sizeof(value), 0, &timeout));
+        check(mq_timedreceive(queue_in, (char*)&flag, sizeof(int), nullptr, &timeout));
 
         if (flag == 1) {
-            guessed = 1;
+            guessed = true;
             printf("Count: %d\n", count);
             printf("Process %d guessed the number: %d\n", getpid(), value);
         } else {
@@ -105,34 +86,35 @@ void right_player(mqd_t queue_out, mqd_t queue_in, pid_t opponent) {
     }
 }
 
-void role_change(int game_count, mqd_t queue_write, mqd_t queue_read, pid_t pid, pid_t pid_p, int range_max) {
+
+void role_change(int game_count, mqd_t queue_write, mqd_t queue_read, int pid, int range_max) {
     int count = 0;
     auto start_time = std::chrono::high_resolution_clock::now();
 
     if (game_count % 2 != 0) {
         if (pid == 0) {
-            left_player(queue_write, queue_read, range_max, pid_p);
+            left_player(queue_write, queue_read, range_max);
         }
         else {
-            right_player(queue_write, queue_read, pid);
+            right_player(queue_write, queue_read);
         }
     }
     else {
         if (pid > 0) {
-            left_player(queue_write, queue_read, range_max, pid);
+            left_player(queue_write, queue_read, range_max);
         }
         else {
-            right_player(queue_write, queue_read, pid_p);
+            right_player(queue_write, queue_read);
         }
     }
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
     std::cout << "Time: " << duration << " ms\n";
+
 }
 
 void start(int rounds, int range_max) {
-    no_zombie();
     mq_attr attr;
     attr.mq_maxmsg = 10;
     attr.mq_msgsize = sizeof(int);
@@ -144,10 +126,8 @@ void start(int rounds, int range_max) {
     mqd_t parent = check(mq_open("/parent_to_child", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attr));
     mqd_t child = check(mq_open("/child_to_parent", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attr));
 
-    pid_t pid_p = getpid();
-    pid_t pid = check(fork());
+    int pid = check(fork());
 
-    srand(pid ? 1000 : 100000 );
     mqd_t queue_write, queue_read;
     if (pid == 0) {
         queue_write = child;
@@ -157,10 +137,9 @@ void start(int rounds, int range_max) {
         queue_write = parent;
         queue_read = child;
     }
-
     for (int game_count = 1; game_count <= rounds; ++game_count) {
         printf("Game %d\n", game_count);
-        role_change(game_count, queue_write, queue_read, pid, pid_p, range_max);
+        role_change(game_count, queue_write, queue_read, pid, range_max);
     }
 
     check(mq_close(parent));
@@ -171,5 +150,7 @@ void start(int rounds, int range_max) {
 }
 
 int main() {
+
     start(200, 10000);
+
 }
